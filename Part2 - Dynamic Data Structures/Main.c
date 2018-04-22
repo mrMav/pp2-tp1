fe6dbb4baca4bb7e1889e76ea88c810123f77a7f
mrmav <mrmav@outlook.pt>
2018-04-22 13:27:58 +0100

    solution and project management

---
+++ Part2 - Dynamic Data Structures/Main.c
@@ -0,0 +1,20 @@
+/*
+author: Jorge Noro
+date: 22/04/2018
+purpose: pp2-tp1, part 2, dynamic data structures
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+	// seed for random numbers
+	srand(time(NULL));   // should only be called once
+		
+
+	return 0;
+
+}
